14f5f5812921c47ecb2c69bc46f5cac0e42b582e
Allan Simon <simona@gobeta.com.cn>
2015-05-01 15:15:21 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Docker image with everything needed to run symfony commands
+
+/!\ it's only to prepare the project / update composer / run symfony commands
+
+for production use, you need then to couple that with a php-fpm / nginx images
---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd /var/www
+
+if [ -z "$1" ];
+    then
+
+    # if you're in china and that for some reason
+    # the docker build has failed to download composer.phar...
+    if ! which composer; then
+        if [ -f composer.phar ]; then
+            cp composer.phar /usr/local/bin/composer
+            chmod +x /usr/local/bin/composer
+        else 
+            curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+        fi
+    fi
+    # variable provided by ansible when launching the docker container
+    mkdir ~/.composer
+    cat > ~/.composer/config.json <<EOS
+{
+    "config": {
+        "github-oauth": { "github.com": "$GITHUB_TOKEN" }
+    }
+}
+EOS
+    # we make sure to start fresh
+    rm app/config/parameters.yml
+    composer install --no-interaction
+    if [ -d app/cache ]; then 
+        rm -rf app/cache/*
+    else
+        mkdir app/cache
+    fi
+    php app/console assets:install --symlink web/
+    php app/console c:c
+    php app/console c:w
+    php app/console doctrine:migrations:migrate --no-interaction
+    php app/console assetic:dump
+    # not pretty ...
+    chmod -R 777 app/logs
+    chmod -R 777 app/cache
+    touch app/ready
+    chmod 666 app/ready
+
+else
+    exec "$@"
+fi
